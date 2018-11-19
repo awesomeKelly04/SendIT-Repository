f9af0f3dee666ef64450e5bd65f39ff2ef9e32a3
Emmanuel Oboh <awesomekelly04@outlook.com>
2018-11-19 02:30:22 +0100

    [chores #162040202] API v1 Restructure to ES6

--- src/validation/dataValidator.js
+++ src/validation/dataValidator.js
@@ -6,34 +6,33 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
-class dataValidator{
+const validateParcel = (parcel) => { 
+    const valid = Joi.string().min(3).required();
+     const schema = {
+         parcelName: valid,
+         parcelWeight: valid, 
+         parcelFee: valid, 
+         collectionAddress: valid, 
+         collectionCity: valid, 
+         collectionState: valid, 
+         collectionDate: valid, 
+         destinationAddress: valid,  
+         destinationCity: valid, 
+         destinationState: valid, 
+         userId: valid
+     };
+    return Joi.validate(parcel, schema);
+};
 
-	static validateParcel = (parcel) => { 
-        const valid = Joi.string().min(3).required();
-         const schema = {
-             parcelName: valid,
-             parcelWeight: valid, 
-             parcelFee: valid, 
-             collectionAddress: valid, 
-             collectionCity: valid, 
-             collectionState: valid, 
-             collectionDate: valid, 
-             destinationAddress: valid,  
-             destinationCity: valid, 
-             destinationState: valid, 
-             userId: valid
-         };
-        return Joi.validate(parcel, schema);
+const validateParcelCancelOrder = (parcel) => { 
+    const valid = Joi.string().min(3).required();
+     const schema = {
+         parcelStatus: valid
     };
-    
-    static validateParcelCancelOrder = (parcel) => { 
-        const valid = Joi.string().min(3).required();
-         const schema = {
-             parcelStatus: valid
-        };
-    
-         return Joi.validate(parcel, schema);
-    };	
-}
 
-export default dataValidator;
+     return Joi.validate(parcel, schema);
+};
+
+export default {
+    validateParcel, validateParcelCancelOrder
+};

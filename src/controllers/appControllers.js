0bf16e96755af7a7c957ee413da2646df28cd114
Emmanuel Oboh <awesomekelly04@outlook.com>
2018-11-20 20:11:06 +0100

    [Chores #162040202] More Refactor on APIv1

--- src/controllers/appControllers.js
+++ src/controllers/appControllers.js
@@ -15,7 +15,7 @@ const options = {
   promiseLib: promise
 };
 
-const pgp = pgPromise(options);
+const pgp = pgPromise(options).native;
 const connectionString = process.env['DATABASE_URL'] || 'postgres://postgres:Awesome$0088@localhost:5432/senditdb';
 const db = pgp(connectionString);
 
